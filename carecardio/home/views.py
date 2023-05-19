49a2138d4ce055b451912cfb56c75311d282d5cc
gozygreg <>
2023-05-19 23:38:08 +0000

    Add contact page

--- carecardio/home/views.py
+++ carecardio/home/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/index.html', {})
+
+
+def contact_us(request):
+    return render(request, 'home/contact_us.html', {})
