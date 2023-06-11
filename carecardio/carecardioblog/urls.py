cde0abf89475dcf5b5b42c6e930b075ea2d2f1c1
gozygreg <goziennabuife@yahoo.com>
2023-06-11 18:44:41 +0000

    create view to handle individual blog posts

--- carecardio/carecardioblog/urls.py
+++ carecardio/carecardioblog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('blog/', views.post_list, name='blog'),
+    path('blog/<int:pk>/', views.blog_detail, name='blog_detail'),
 ]
--- carecardio/carecardioblog/views.py
+++ carecardio/carecardioblog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
 
 
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'carecardioblog/blog.html', {})
+    return render(request, 'carecardioblog/blog.html', {'posts': posts})
+
+
+def blog_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'home/blog_detail.html', {'post': post})
